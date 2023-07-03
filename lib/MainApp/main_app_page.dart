0d71b9b063f4cc1ce63f366a985e18c8e74f1bf5
Markymark <mark.pc.lee@gmail.com>
2023-07-04 02:25:58 +0800

    Basic Nav Bar setup

---
+++ lib/MainApp/main_app_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:ventes/MainApp/Chat/main_chat_page.dart';
+import 'package:ventes/MainApp/Profile/main_profile_page.dart';
+import 'package:ventes/MainApp/Search/main_search_page.dart';
+
+class MainAppPage extends StatefulWidget {
+  const MainAppPage({super.key});
+
+  @override
+  State<MainAppPage> createState() => _MainAppPageState();
+}
+
+class _MainAppPageState extends State<MainAppPage> {
+  var selectedIndex = 1;
+
+  onItemTapped(int index) {
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  List<Widget> pages = const [
+    SearchPage(),
+    ProfilePage(),
+    ChatPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: pages[selectedIndex],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+          BottomNavigationBarItem(icon: Icon(Icons.message), label: "Chat"),
+        ],
+        currentIndex: selectedIndex,
+        onTap: onItemTapped,
+      ),
+    );
+  }
+}
